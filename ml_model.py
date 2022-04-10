0795fff0f3b20a9b912517a1ad09c6b9d5033967
mrinalbanerji <mrinalbanerji12@gmail.com>
2022-04-10 09:17:56 +0530

    added 3 new model codes

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,14 @@
 
 
 print("adding all the modules for the ml model")
+
+def model1():
+    print("model 1 is random forest")
+
+def model2():
+    print("model 2 is Logistic Regression")
+
+def model3():
+    print("model 3 is XG Boost classifier")
+
+
